860d2daf866c2f167f99e88b66ea8ccd18b233e2
Divya kotte <153913545+divya462@users.noreply.github.com>
2025-09-10 15:37:45 +0530

    Update and rename Employee salary program.java to EmployeeTest.java

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,3 +1,12 @@
+/*  
+1) Write a program by creating an Employee class having the following methods and print
+the final salary.
+1 - getInfo(); which takes the salary, number of hours of work per day of employee as
+parameter
+2 - addSal(); which adds $10 to salary of the employee if it is less than $500.
+3 - addWork(); which adds $5 to salary of employee if the number of hours of work per
+day is more than 6 hours.
+*/
 import java.util.Scanner;
 
 class Employee {
